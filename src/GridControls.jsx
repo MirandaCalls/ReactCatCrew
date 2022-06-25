d09a671f35bae8cb2c100b820a31df83eab54256
SecondSerenity <gmaidenmueller@gmail.com>
2022-06-24 21:04:06 -0500

    Experimental responsive search box

--- src/GridControls.jsx
+++ src/GridControls.jsx
@@ -1,9 +1,15 @@
 import ToggleInput from "./ToggleInput.jsx";
 import SearchInput from "./SearchInput.jsx";
+import MagnifyingGlass from "./MagnifyingGlass.jsx";
+import useWindowWidth from "./useWindowWidth.js";
 
 import "./GridControls.css";
 
-const GridControls = ({ displayMobile, onlyOnDuty, searchText, onChange }) => {
+const GridControls = ({ onlyOnDuty, searchText, onChange }) => {
+  const [width] = useWindowWidth();
+
+  const displayMobile = width < 552;
+
   return (
     <div className="grid-controls-bar">
       <div className="grid-controls-bar__container">
@@ -14,12 +20,30 @@ const GridControls = ({ displayMobile, onlyOnDuty, searchText, onChange }) => {
             onChange(isOn, searchText);
           }}
         />
-        <SearchInput
-          color="#ddf2ff"
-          onChange={(text) => {
-            onChange(onlyOnDuty, text);
-          }}
-        />
+        {displayMobile ? (
+          <div
+            style={{
+              margin: "0px 10px",
+              border: "1px solid black",
+              padding: "8px 8px",
+              display: "flex",
+              "flex-direction": "column",
+              "align-items": "center",
+              "border-radius": "5px",
+              "background-color": "#ddf2ff",
+              cursor: "pointer",
+            }}
+          >
+            <MagnifyingGlass style />
+          </div>
+        ) : (
+          <SearchInput
+            color="#ddf2ff"
+            onChange={(text) => {
+              onChange(onlyOnDuty, text);
+            }}
+          />
+        )}
       </div>
     </div>
   );
